2c53334ead57cdcc144368441f94f934ae4c1b45
codystack <ebuzzadvert@gmail.com>
2020-06-24 03:53:36 +0100

    new files

---
+++ components/lightheader.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<head>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-52115242-16"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'UA-52115242-16');
+    </script>
+    <!-- Google Tag Manager -->
+    <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MTCQKMK');
+    </script>
+    <!-- End Google Tag Manager -->
+    <meta charset="utf-8">
+    <title>KEG&trade; :: Gasoline in your pocket.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A corporate Bootstrap theme by Medium Rare">
+    <link rel="shortcut icon" type="image/x-icon" href="https://i.imgur.com/zvUvUF7.png">
+    <style>
+      @keyframes hideLoader{0%{ width: 100%; height: 100%; }100%{ width: 0; height: 0; }  }  body > div.loader{ position: fixed; background: white; width: 100%; height: 100%; z-index: 1071; opacity: 0; transition: opacity .5s ease; overflow: hidden; pointer-events: none; display: flex; align-items: center; justify-content: center;}body:not(.loaded) > div.loader{ opacity: 1;}body:not(.loaded){ overflow: hidden;}  body.loaded > div.loader{animation: hideLoader .5s linear .5s forwards;  } /* Typing Animation */.loading-animation {width: 6px;height: 6px;border-radius: 50%;animation: typing 1s linear infinite alternate;position: relative;left: -12px;}@keyframes typing {0% {background-color: rgba(100,100,100, 1);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 0.2),24px 0px 0px 0px rgba(100,100,100, 0.2);}25% {background-color: rgba(100,100,100, 0.4);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 2),24px 0px 0px 0px rgba(100,100,100, 0.2);}75% {background-color: rgba(100,100,100, 0.4);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 0.2),24px 0px 0px 0px rgba(100,100,100, 1);}}
+    </style>
+    <script type="text/javascript">
+      window.addEventListener("load", function () {    document.querySelector('body').classList.add('loaded');  });
+    </script>
+    <link href="assets/css/theme-mobile-app.css" rel="stylesheet" type="text/css" media="all" />
+    <link rel="preload" as="font" href="assets/fonts/Inter-UI-upright.var.woff2" type="font/woff2" crossorigin="anonymous">
+    <link rel="preload" as="font" href="assets/fonts/Inter-UI.var.woff2" type="font/woff2" crossorigin="anonymous">
+  </head>
+
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MTCQKMK"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <div class="loader">
+      <div class="loading-animation"></div>
+    </div>
+    <?php include('./components/modals.php');?>
+
+    <div class="navbar-container ">
+      <nav class="navbar navbar-expand-lg navbar-light border-bottom-0 bg-white" data-sticky="top">
+        <div class="container">
+            <a class="navbar-brand fade-page" href="\">
+              <img src="https://i.imgur.com/VrtB6Jx.png" alt="Keg" style="width: 100px;">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+              <img class="icon navbar-toggler-open" src="assets/img/icons/interface/menu.svg" alt="menu interface icon" data-inject-svg />
+              <img class="icon navbar-toggler-close" src="assets/img/icons/interface/cross.svg" alt="cross interface icon" data-inject-svg />
+            </button>
+          <div class="collapse navbar-collapse">
+            <div class="py-2 py-lg-0">
+              <ul class="navbar-nav">
+              <li class="nav-item dropdown">
+                  <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown-grid" aria-expanded="false" aria-haspopup="true">Company</a>
+                  <div class="dropdown-menu row">
+                    <div class="col-auto" data-dropdown-content>
+                      <div class="dropdown-grid-menu">
+                          <a href="about" class="dropdown-item fade-page">About us</a>
+                          <a href="how-it-works" class="dropdown-item">How it works</a>
+                          <a href="careers" class="dropdown-item">Careers</a>
+                          <a href="careers" class="dropdown-item">Stories</a>
+                          <a href="blog" class="dropdown-item">Blog</a>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li class="nav-item dropdown">
+                  <a href="help" class="nav-link">Help</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a href="corona-virus" class="nav-link">COVID-19 resources</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="collapse navbar-collapse justify-content-end">
+            <div class="py-2 py-lg-0">
+              <ul class="navbar-nav">
+              <li class="nav-item dropdown">
+                  <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown-grid" aria-expanded="false" aria-haspopup="true"><img class="icon bg-dark" style="height: 22px;" src="assets/img/icons/theme/layout/layout-grid.svg" alt="decoration" data-inject-svg />Products</a>
+                  <div class="dropdown-menu row">
+                    <div class="col-auto" data-dropdown-content>
+                      <div class="dropdown-grid-menu">
+                          <a href="https://www.powerup.keg.com.ng" target="_blank" class="dropdown-item fade-page">Power-Up<span class="badge badge-primary ml-2">New</span></a>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <div data-target="#login-modal" data-toggle="modal">
+                  <a href="#" class="nav-link"><img class="icon bg-dark style="height: 22px;" src="assets/img/icons/theme/general/user.svg" alt="decoration" data-inject-svg />Log in</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div data-target="#signup-modal" data-toggle="modal">
+            <a href="#" class="btn btn-index-signup ml-lg-3">Sign up</a>
+
+          </div>
+        </div>
+      </nav>
+    </div>
---
+++ components/modals.php
@@ -0,0 +1,81 @@
+          <!-- Login Modal End -->
+            <div class="m-1">
+              <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                  <div class="modal-content">
+                    <div class="modal-body">
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <img class="icon bg-dark" src="assets/img/icons/interface/cross.svg" alt="cross interface icon" data-inject-svg />
+                      </button>
+                      <div class="m-xl-4 m-3">
+                        <div class="icon-round icon-round-lg bg-primary mx-auto mb-4">
+                          <img class="icon bg-dark" src="assets/img/icons/theme/general/user.svg" alt="decoration" data-inject-svg /></div>
+                        </div>
+                        <div class="text-center mb-4">
+                          <h4 class="h3 mb-1">Login Account</h4>
+                          <p>Choose login type.</p>
+                        </div>
+                          <div class="form-row">
+                            <div class="col-12">
+                              <div class=" d-flex flex-wrap loogin justify-content-center">
+                              <a href="./auth/dispatch/login" target="_blank" class="mx-2 btn btn-primary mb-3">
+                                <span>Dispatcher Login</span>
+                                <img class="icon" src="assets/img/icons/theme/navigation/sign-in.svg" alt="decoration" data-inject-svg />
+                              </a>
+                              <a href="./auth/user/login" target="_blank" class="mx-2 btn btn-primary mb-3">
+                                <span>User Login</span>
+                                <img class="icon" src="assets/img/icons/theme/navigation/sign-in.svg" alt="decoration" data-inject-svg />
+                              </a>
+                              </div>
+                            </div>
+                          </div>
+                        </small>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          <!-- Login Modal End -->
+
+          <!-- Sign up Modal End -->
+            <div class="m-1">
+              <div class="modal fade" id="signup-modal" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                  <div class="modal-content">
+                    <div class="modal-body">
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <img class="icon bg-dark" src="assets/img/icons/interface/cross.svg" alt="cross interface icon" data-inject-svg />
+                      </button>
+                      <div class="m-xl-4 m-3">
+                        <div class="icon-round icon-round-lg bg-primary mx-auto mb-4">
+                        <img class="icon bg-dark" src="assets/img/icons/theme/communication/add-user.svg" alt="decoration" data-inject-svg /></div>
+                        </div>
+                        <div class="text-center mb-4">
+                          <h4 class="h3 mb-1">Create Account</h4>
+                          <p>Choose account type.</p>
+                        </div>
+                          <div class="form-row">
+                            <div class="col-12">
+                              <div class=" d-flex flex-wrap loogin justify-content-center">
+                              <a href="./auth/dispatch/sign-up" target="_blank" class="mx-2 btn btn-primary mb-3">
+                                <span>Dispatcher Sign up</span>
+                                <img class="icon" src="assets/img/icons/theme/navigation/angle-right.svg" alt="decoration" data-inject-svg />
+                              </a>
+                              <a href="./auth/user/sign-up" target="_blank" class="mx-2 btn btn-primary mb-3">
+                                <span>User Sign up</span>
+                                <img class="icon" src="assets/img/icons/theme/navigation/angle-right.svg" alt="decoration" data-inject-svg />
+                              </a>
+                              </div>
+                            </div>
+                          </div>
+                        </small>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          <!-- Sign up Modal End -->
