740b503df55625495e8d98f7cd2882d1a0957e4a
codystack <ebuzzadvert@gmail.com>
2020-08-01 15:05:12 +0100

    ,new ui added

--- components/header.php
+++ components/header.php
@@ -1,114 +1,61 @@
 <!doctype html>
-<html lang="en">
-  
-<meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
-<head>
+    <html lang="en">
 
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-52115242-16"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'UA-52115242-16');
-    </script>
-    <!-- Google Tag Manager -->
-    <script>
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-MTCQKMK');
-    </script>
-    <!-- End Google Tag Manager -->
-    <meta charset="utf-8">
-    <title>KEG&trade; :: Gasoline in your pocket.</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="A corporate Bootstrap theme by Medium Rare">
-    <link rel="shortcut icon" type="image/x-icon" href="https://i.imgur.com/zvUvUF7.png">
-    <style>
-      @keyframes hideLoader{0%{ width: 100%; height: 100%; }100%{ width: 0; height: 0; }  }  body > div.loader{ position: fixed; background: white; width: 100%; height: 100%; z-index: 1071; opacity: 0; transition: opacity .5s ease; overflow: hidden; pointer-events: none; display: flex; align-items: center; justify-content: center;}body:not(.loaded) > div.loader{ opacity: 1;}body:not(.loaded){ overflow: hidden;}  body.loaded > div.loader{animation: hideLoader .5s linear .5s forwards;  } /* Typing Animation */.loading-animation {width: 6px;height: 6px;border-radius: 50%;animation: typing 1s linear infinite alternate;position: relative;left: -12px;}@keyframes typing {0% {background-color: rgba(100,100,100, 1);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 0.2),24px 0px 0px 0px rgba(100,100,100, 0.2);}25% {background-color: rgba(100,100,100, 0.4);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 2),24px 0px 0px 0px rgba(100,100,100, 0.2);}75% {background-color: rgba(100,100,100, 0.4);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 0.2),24px 0px 0px 0px rgba(100,100,100, 1);}}
-    </style>
-    <script type="text/javascript">
-      window.addEventListener("load", function () {    document.querySelector('body').classList.add('loaded');  });
-    </script>
-    <link href="assets/css/style.css" rel="stylesheet" type="text/css" media="all" />
-    <link rel="preload" as="font" href="assets/fonts/Inter-UI-upright.var.woff2" type="font/woff2" crossorigin="anonymous">
-    <link rel="preload" as="font" href="assets/fonts/Inter-UI.var.woff2" type="font/woff2" crossorigin="anonymous">
-  </head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <head>
+        <!-- Google Tag Manager -->
+        <!-- End Google Tag Manager -->
+        <meta charset="utf-8">
+        <title>PowerUp&reg; :: Buy Electricity Online</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="keywords" content="Buy electricity online. Prepaid electricity. Prepaid electricity units. How to Buy power. Effortless PowerUp online. Pay electricity bills Easy, Effortless and Simple. buy power aedc. Nepa PHCN units online. Card USSD bank payment. PowerUp nigeria. PowerUp abuja. buy power ng. Pay as you go electricity. Recharge Electricity Abuja. Recharge Electricity Nigeria. Online purchase for Abuja Disco. Prepaid meter tokens, Electricity online in nigeria. Recharge your meter in nigeria. PHCN meter recharge. NEPA meter recharge. Online electricity pin recharge. Purchase electricity, PowerUp in nigeria, Electricity pins, Electricity tokens, light, purchase light online, Nigerian electricity">
+        <meta name="description" content="Pay Electricity Bills Online in Nigeria on PowerUp. Buy Prepaid and Postpaid Meter units for AEDC, JED, EKEDC, IBEDC  Ikeja Electric and Kaduna Electricity. Buy Electricity form the comfort of your home"/><meta property="og:description" content="Pay Electricity Bills Online in Nigeria on PowerUp. Buy Prepaid and Postpaid Meter units for AEDC, JED, EKEDC, IBEDC  Ikeja Electric and Kaduna Electricity. Buy Electricity form the comfort of your home">
+        <style>
+            @keyframes hideLoader{0%{ width: 100%; height: 100%; }100%{ width: 0; height: 0; }  }  body > div.loader{ position: fixed; background: white; width: 100%; height: 100%; z-index: 1071; opacity: 0; transition: opacity .5s ease; overflow: hidden; pointer-events: none; display: flex; align-items: center; justify-content: center;}body:not(.loaded) > div.loader{ opacity: 1;}body:not(.loaded){ overflow: hidden;}  body.loaded > div.loader{animation: hideLoader .5s linear .5s forwards;  } /* Typing Animation */.loading-animation {width: 6px;height: 6px;border-radius: 50%;animation: typing 1s linear infinite alternate;position: relative;left: -12px;}@keyframes typing {0% {background-color: rgba(100,100,100, 1);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 0.2),24px 0px 0px 0px rgba(100,100,100, 0.2);}25% {background-color: rgba(100,100,100, 0.4);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 2),24px 0px 0px 0px rgba(100,100,100, 0.2);}75% {background-color: rgba(100,100,100, 0.4);box-shadow: 12px 0px 0px 0px rgba(100,100,100, 0.2),24px 0px 0px 0px rgba(100,100,100, 1);}}
+        </style>
+        <script type="text/javascript">
+            window.addEventListener("load", function () {    document.querySelector('body').classList.add('loaded');  });
+        </script>
+        <link href="assets/css/style.css" rel="stylesheet" type="text/css" media="all" />
+        <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+        <link rel="preload" as="font" href="assets/fonts/Inter-UI-upright.var.woff2" type="font/woff2" crossorigin="anonymous">
+        <link rel="preload" as="font" href="assets/fonts/Inter-UI.var.woff2" type="font/woff2" crossorigin="anonymous">
+    </head>
 
-  <body>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MTCQKMK"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
+    <body>
     <div class="loader">
-      <div class="loading-animation"></div>
+        <div class="loading-animation"></div>
     </div>
-    <?php include('./components/modals.php');?>
 
-    <div class="navbar-container">
-      <nav class="navbar navbar-expand-lg navbar-dark" data-overlay data-sticky="top">
-        <div class="container">
-          <a class="navbar-brand fade-page" href="\">
-            <img src="https://i.imgur.com/3UcjRE8.png" alt="Keg" style="width: 100px;">
-          </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
-            <img class="icon navbar-toggler-open" src="assets/img/icons/interface/menu.svg" alt="menu interface icon" data-inject-svg />
-            <img class="icon navbar-toggler-close" src="assets/img/icons/interface/cross.svg" alt="cross interface icon" data-inject-svg />
-          </button>
-          <div class="collapse navbar-collapse">
-            <div class="py-2 py-lg-0">
-              <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                  <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown-grid" aria-expanded="false" aria-haspopup="true">Company</a>
-                  <div class="dropdown-menu row">
-                    <div class="col-auto" data-dropdown-content>
-                      <div class="dropdown-grid-menu">
-                          <a href="about" class="dropdown-item fade-page">About us</a>
-                          <a href="how-it-works" class="dropdown-item">How it works</a>
-                          <a href="careers" class="dropdown-item">Careers</a>
-                          <a href="careers" class="dropdown-item">Stories</a>
-                          <a href="blog" class="dropdown-item">Blog</a>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li class="nav-item dropdown">
-                  <a href="help" class="nav-link">Help</a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a href="corona-virus" class="nav-link">COVID-19 resources</a>
-                </li>
-              </ul>
-            </div>
+    <div class="navbar-container bg-primary">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary" data-sticky="top">
+            <div class="container">
+                <a class="navbar-brand fade-page" href="\">
+                    <img src="assets/img/logo1.svg" alt="PowerUp" width="202" height="70">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <img class="icon navbar-toggler-open" src="assets/img/icons/interface/menu.svg" alt="menu interface icon" data-inject-svg />
+                    <img class="icon navbar-toggler-close" src="assets/img/icons/interface/cross.svg" alt="cross interface icon" data-inject-svg />
+                </button>
+                <div class="collapse navbar-collapse justify-content-end">
+                    <div class="py-2 py-lg-0">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="login" class="nav-link">How it works</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="login" class="nav-link">FAQ's</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="login" class="nav-link">Support</a>
+                            </li>
+                            <li class="nav-item">
+                                    <a href="login" class="nav-link"><img class="icon bg-white" style="height: 22px;" src="assets/img/icons/theme/general/user.svg" alt="login" data-inject-svg />Log in</a>
+                            </li>
+                        </ul>
+                    </div><a href="register" class="btn btn-white">Register</a>
 
-          </div>
-          <div class="collapse navbar-collapse justify-content-end">
-            <div class="py-2 py-lg-0">
-              <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                  <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown-grid" aria-expanded="false" aria-haspopup="true"><img class="icon bg-white" style="height: 22px;" src="assets/img/icons/theme/layout/layout-grid.svg" alt="decoration" data-inject-svg />Products</a>
-                  <div class="dropdown-menu row">
-                    <div class="col-auto" data-dropdown-content>
-                      <div class="dropdown-grid-menu">
-                          <a href="https://www.powerup.keg.com.ng" target="_blank" class="dropdown-item fade-page">Power-Up<span class="badge badge-primary ml-2">New</span></a>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li class="nav-item">
-                  <div data-target="#login-modal" data-toggle="modal">
-                  <a href="#" class="nav-link"><img class="icon bg-white" style="height: 22px;" src="assets/img/icons/theme/general/user.svg" alt="decoration" data-inject-svg />Log in</a>
-                  </div>
-                </li>
-              </ul>
+                </div>
             </div>
-            <div data-target="#signup-modal" data-toggle="modal">
-            <a href="#" class="btn btn-signup ml-lg-3">Sign up</a>
-            </div>
-          </div>
-        </div>
-      </nav>
+        </nav>
     </div>
